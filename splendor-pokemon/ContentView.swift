414fd261514f021bad633802994c465e208a56f8
nimuseel <l08c0739@gmail.com>
2024-07-29 17:12:34 +0900

    Initial Commit

---
+++ splendor-pokemon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  splendor-pokemon
+//
+//  Created by 이수민 on 7/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
