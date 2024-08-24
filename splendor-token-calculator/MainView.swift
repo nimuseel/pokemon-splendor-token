961f9e92b4d81055e405abca9f351af69f821697
nimuseel <l08c0739@gmail.com>
2024-08-24 16:31:39 +0900

    feat: 프로젝트 네이밍 변경 및 인원 선택 스텝 추가

---
+++ splendor-token-calculator/MainView.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+
+struct MainView: View {
+    @State private var showAlert = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                HStack(alignment: .center, spacing: 12) {
+                    NavigationLink(destination: ContentView()) {
+                        Text("2인")
+                            .frame(width: 150, height: 80)
+                            .foregroundColor(.primary)
+                            .overlay(
+                              RoundedRectangle(cornerRadius: 12)
+                                .stroke(.gray, lineWidth:1)
+                            )
+                    }
+                    
+                    Button("3인 이상") {
+                        showAlert = true
+                    }
+                        .frame(width: 150, height: 80)
+                        .foregroundColor(.primary)
+                        .overlay(
+                          RoundedRectangle(cornerRadius: 12)
+                            .stroke(.gray, lineWidth:1)
+                        )
+                        .alert("3인 이상 규칙은 열심히 개발 중입니다! 조금만 기다려 주세요!", isPresented: $showAlert) {
+                            Button("OK", role: .cancel) {
+                                showAlert = false
+                            }
+                        }
+                    
+                }
+                Rectangle()
+                    .frame(width: .infinity, height: 170)
+                    .opacity(0)
+            }
+            .navigationTitle("인원 선택")
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ splendor-token-calculator/splendor_token_calculatorApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct splendor_token_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
